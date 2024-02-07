313db28654e6469b363dc4b21fb49c11cb1fd478
Pablo <st.spasam@gmail.com>
2024-02-07 20:17:29 +0200

    Удалены файлы App.css, App.jsx и изменен путь к компоненте App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+
+import { Product } from "./Product";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+      <Product />
+    </div>
+  );
+}

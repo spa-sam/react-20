035dac8018a550cf5bc7b64438e2ac5e27386409
Pablo <st.spasam@gmail.com>
2024-02-09 22:11:38 +0200

    Добавлены зависимости clsx, modern-normalize и react-icons. Удалены файлы App.css и App.jsx. Обновлены стили в index.css.

---
+++ src/components/Alert.jsx
@@ -0,0 +1,17 @@
+// src/components/Alert.jsx
+
+import clsx from "clsx";
+import "./Alert.css";
+
+export const Alert = ({ variant, outlined, elevated, children }) => {
+  return (
+    <p
+      className={clsx("alert", variant, {
+        "is-outlined": outlined,
+        "is-elevated": elevated,
+      })}
+    >
+      {children}
+    </p>
+  );
+};
---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import css from "./App.module.css";
+console.log(css);
+
+export const App = () => {
+  return (
+    <>
+      <h1 className={css.titel}>Paymants</h1>
+    </>
+  );
+};
